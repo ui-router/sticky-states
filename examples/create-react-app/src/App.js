07111877ecf831db688fc8b3432c8d5fb2bad3dd
Chris Thielen <cthielen@netflix.com>
2018-01-13 16:36:27 -0800

    chore(example): Add create-react-app example

---
+++ examples/create-react-app/src/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { UIRouter, UISref, UISrefActive, UIView } from '@uirouter/react';
+import { pushStateLocationPlugin } from '@uirouter/core';
+import { Visualizer } from '@uirouter/visualizer';
+import { StickyStatesPlugin } from '@uirouter/sticky-states';
+
+import GenericCmp from './Generic';
+import './App.css';
+
+const plugins = [
+  pushStateLocationPlugin,
+  Visualizer,
+  StickyStatesPlugin,
+]
+
+const states = [
+  { 
+    name: 'home',
+    url: '/home',
+    sticky: true,
+    views: {
+      home: { component: GenericCmp },
+    }, 
+  },
+
+  {
+    name: 'about',
+    url: '/about',
+    sticky: true,
+    views: {
+      about: { component: GenericCmp },
+    },
+  },
+]; 
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentState: null
+    }
+  }
+
+  routerConfig(router) {
+    this.unsub = router.transitionService.onSuccess({}, (transition) => {
+      this.setState({ currentRouterState: transition.to().name })
+      console.log(transition)
+    })
+  }
+  
+  componentWillUnmount() {
+    this.unsub && this.unsub();
+  }
+
+  render() {
+    const current = this.state.currentRouterState;
+    const showHideStyle = (statename) => ({
+      display: current === statename ? 'block' : 'none'
+    })
+
+    return (
+      <UIRouter plugins={plugins} states={states} config={router => this.routerConfig(router)}>
+        <div>
+          <UISrefActive class="active">
+            <UISref to="home"><a>home</a></UISref>
+          </UISrefActive>
+
+          <UISrefActive class="active">
+            <UISref to="about"><a>about</a></UISref>
+          </UISrefActive>
+
+          <div style={showHideStyle('home')}>
+            <UIView name="home"/>
+          </div>
+
+          <div style={showHideStyle('about')}>
+            <UIView name="about"/>
+          </div>
+        </div>
+      </UIRouter>
+    );
+  }
+}
+
+export default App;
---
+++ examples/create-react-app/src/Generic.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class GenericCmp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "Text entered here is not lost",
+      stateName: props.resolves.$state$.name,
+    }
+  }
+
+  handleChange(input) {
+      this.setState({ text: input.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.stateName} state loaded</h1>
+        <textarea 
+          value={this.state.text} 
+          onChange={(input) => this.setState({ text: input.target.value })}
+        />
+      </div>
+    );
+  }
+}
+
+export default GenericCmp;
