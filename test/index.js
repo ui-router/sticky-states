9b170b0a4453e7bd99c34f02a7b2251c327cd979
Chris Thielen <cthielen@netflix.com>
2020-04-29 13:30:37 -0700

    chore(package): update dependencies

--- test/index.js
+++ test/index.js
@@ -1,7 +1,6 @@
 // require all source files ending in "Spec" from the
 // current directory and all subdirectories
 
-require('core-js');
 require('@uirouter/core');
 require('../src/stickyStates');
 
