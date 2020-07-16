ebb722e2bc4f7301c83ad843be635e19f03e3d92
Chris Thielen <cthielen@netflix.com>
2020-07-15 20:38:42 -0700

    refactor(examples): convert react App component to a Functional component

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { UIRouter, useSrefActive, UIView } from '@uirouter/react';
+import * as React from 'react';
+import { UIRouter, useSrefActive, useRouter, UIView } from '@uirouter/react';
 import { pushStateLocationPlugin } from '@uirouter/core';
 import { Visualizer } from '@uirouter/visualizer';
 import { StickyStatesPlugin } from '@uirouter/sticky-states';
@@ -29,52 +29,45 @@ const states = [
   },
 ];
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentState: null,
-    };
-  }
-
-  routerConfig(router) {
-    router.urlService.rules.initial({ state: 'home' });
-    this.unsub = router.transitionService.onSuccess({}, (transition) => {
-      this.setState({ currentRouterState: transition.to().name });
-      console.log(transition);
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsub && this.unsub();
-  }
-
-  render() {
-    const current = this.state.currentRouterState;
-    const showHideStyle = (statename) => ({
-      display: current === statename ? 'block' : 'none',
-    });
-
-    const homeSref = useSrefActive('home', null, 'active');
-    const aboutSref = useSrefActive('about', null, 'active');
-
-    return (
-      <UIRouter plugins={plugins} states={states} config={(router) => this.routerConfig(router)}>
-        <div>
-          <a {...homeSref}>home</a>
-          <a {...aboutSref}>about</a>
-
-          <div style={showHideStyle('home')} id="home">
-            <UIView name="home" />
-          </div>
-
-          <div style={showHideStyle('about')} id="about">
-            <UIView name="about" />
-          </div>
-        </div>
-      </UIRouter>
-    );
-  }
+function routerConfig(router) {
+  router.urlService.rules.initial({ state: 'home' });
 }
 
-export default App;
+function AppRoot() {
+  return (
+    <UIRouter plugins={plugins} states={states} config={routerConfig}>
+      <App />
+    </UIRouter>
+  );
+}
+
+function App() {
+  const globals = useRouter().globals;
+
+  const currentState = globals.current.name;
+
+  const showHideStyle = (statename) => ({
+    display: currentState === statename ? 'block' : 'none',
+  });
+
+  const homeSref = useSrefActive('home', null, 'active');
+  const aboutSref = useSrefActive('about', null, 'active');
+
+  return (
+    <div>
+      {currentState}
+      <a {...homeSref}>home</a>
+      <a {...aboutSref}>about</a>
+
+      <div style={showHideStyle('home')} id="home">
+        <UIView name="home" />
+      </div>
+
+      <div style={showHideStyle('about')} id="about">
+        <UIView name="about" />
+      </div>
+    </div>
+  );
+}
+
+export default AppRoot;
--- examples/create-react-app/src/index.js
+++ examples/create-react-app/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AppRoot from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppRoot />, document.getElementById('root'));
