b2986cc03e350bc07d8524fa02a63043da854b82
Chris Thielen <cthielen@netflix.com>
2026-01-02 22:17:41 -1000

    chore: migrate examples from cypress to playwright

--- examples/angularjs-webpack/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:4000',
-    supportFile: false,
-    video: false,
-  },
-});
