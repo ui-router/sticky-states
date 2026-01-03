a6ed751dda01e91e23838f9b12f8e749ee8e5b2f
Chris Thielen <cthielen@netflix.com>
2026-01-02 22:17:41 -1000

    chore: migrate examples from cypress to playwright

---
+++ examples/angular-cli/e2e/example.spec.js
@@ -0,0 +1,52 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('example app', () => {
+  test('loads', async ({ page }) => {
+    await page.goto('/');
+  });
+
+  test('renders links', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.locator('a', { hasText: 'home' })).toBeVisible();
+    await expect(page.locator('a', { hasText: 'about' })).toBeVisible();
+  });
+
+  test('home state has a textarea', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.locator('ui-view[name=home] textarea')).toBeVisible();
+  });
+
+  test('retains text entered into the textarea in home', async ({ page }) => {
+    await page.goto('/');
+    await page.locator('ui-view[name=home] textarea').fill('Text entered here is not lost The quick brown fox');
+
+    await page.locator('a', { hasText: 'about' }).click();
+    await expect(page.getByText('about state loaded')).toBeVisible();
+
+    await page.locator('a', { hasText: 'home' }).click();
+    await expect(page.getByText('home state loaded')).toBeVisible();
+
+    await expect(page.locator('ui-view[name=home] textarea')).toHaveValue(
+      'Text entered here is not lost The quick brown fox'
+    );
+  });
+
+  test('retains text entered into the textarea in about', async ({ page }) => {
+    await page.goto('/');
+
+    await page.locator('a', { hasText: 'about' }).click();
+    await expect(page.getByText('about state loaded')).toBeVisible();
+
+    await page.locator('ui-view[name=about] textarea').fill('Text entered here is not lost The quack white duck');
+
+    await page.locator('a', { hasText: 'home' }).click();
+    await expect(page.getByText('home state loaded')).toBeVisible();
+
+    await page.locator('a', { hasText: 'about' }).click();
+    await expect(page.getByText('about state loaded')).toBeVisible();
+
+    await expect(page.locator('ui-view[name=about] textarea')).toHaveValue(
+      'Text entered here is not lost The quack white duck'
+    );
+  });
+});
