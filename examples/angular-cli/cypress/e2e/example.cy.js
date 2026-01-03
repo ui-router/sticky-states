b1fb941fe0056f4284b8d9fa86dad41e9aa20e7b
Chris Thielen <cthielen@netflix.com>
2026-01-02 21:55:44 -1000

    chore: migrate examples/angular-cli to ng20

--- examples/angular-cli/cypress/e2e/example.cy.js
+++ examples/angular-cli/cypress/e2e/example.cy.js
@@ -16,21 +16,12 @@ describe('example app', () => {
 
   it('retains text entered into the textarea in home', () => {
     cy.visit('');
-    cy
-      .get('ui-view[name=home]')
-      .get('textarea')
-      .type(' The quick brown fox');
+    cy.get('ui-view[name=home]').get('textarea').type(' The quick brown fox');
 
-    cy
-      .get('a')
-      .contains('about')
-      .click();
+    cy.get('a').contains('about').click();
     cy.contains('about state loaded');
 
-    cy
-      .get('a')
-      .contains('home')
-      .click();
+    cy.get('a').contains('home').click();
     cy.contains('home state loaded');
 
     cy.get('ui-view[name=home] textarea').should('have.value', 'Text entered here is not lost The quick brown fox');
@@ -39,24 +30,15 @@ describe('example app', () => {
   it('retains text entered into the textarea in about', () => {
     cy.visit('');
 
-    cy
-      .get('a')
-      .contains('about')
-      .click();
+    cy.get('a').contains('about').click();
     cy.contains('about state loaded');
 
     cy.get('ui-view[name=about] textarea').type(' The quack white duck');
 
-    cy
-      .get('a')
-      .contains('home')
-      .click();
+    cy.get('a').contains('home').click();
     cy.contains('home state loaded');
 
-    cy
-      .get('a')
-      .contains('about')
-      .click();
+    cy.get('a').contains('about').click();
     cy.contains('about state loaded');
 
     cy.get('ui-view[name=about] textarea').should('have.value', 'Text entered here is not lost The quack white duck');
